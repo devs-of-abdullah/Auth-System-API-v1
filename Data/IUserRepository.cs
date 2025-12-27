316486fa7db0f457ecd740afb237972e9d5d4762
Ahmet Abdullah <realahmetabdullah@gmail.com>
2025-12-28 02:46:44 +0300

    Add project files.

---
+++ Data/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+
+namespace Data
+{
+    public interface IUserRepository
+    {
+        Task<UserDto?> GetByEmail(string email);
+        Task<UserDto?> GetById(int id);
+
+        Task<bool> IsExistsById(int id);
+        Task<bool> IsExistsByEmail(string email);
+        Task Add (RegisterUserDto user);
+    }
+}
---
+++ Entities/UserDTOs.cs
@@ -0,0 +1,24 @@
+﻿namespace Entities
+{
+   
+    
+        public class RegisterUserDto
+        {
+            public string Email { get; set; } = null!;
+            public string Password { get; set; } = null!;
+
+        }
+        public class LoginUserDto
+        {
+            public string Email { get; set; } = null!;
+            public string Password { get; set; } = null!;
+        }
+      
+        public class UserDto
+        {
+            public int Id { get; set; }
+            public string Email { get; set; } = null!;
+            public string? Password { get; set; } = null!;
+        }
+     
+}
